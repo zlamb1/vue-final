0f2ab467229309eb901545c16ab9295ee2193c6b
zlamb1 <zlamb@my.wctc.edu>
2023-12-06 13:48:50 -0600

    added search bar to split view

--- components/list/SplitMediaList.vue
+++ components/list/SplitMediaList.vue
@@ -11,7 +11,12 @@ const props = defineProps({
     }
 });
 
+const search = ref('');
 const selected = ref(props.mediaCollection?.length > 0 ? props.mediaCollection[0] : null);
+
+const computedList = computed(() => {
+   return props.mediaCollection.filter((mediaItem) => mediaItem?.media?.title?.includes(search.value));
+});
 
 function trimTitle(title) {
     const trimAmounts = {
@@ -32,15 +37,27 @@ function trimTitle(title) {
 <template>
     <q-card class="row container no-wrap">
         <div class="col-4 col-md-3 col-lg-2 left">
-            <q-list class="" style="height: 500px" separator>
-                <q-item v-for="mediaItem in mediaCollection"
-                        active-class="bg-primary text-white"
-                        :active="mediaItem === selected"
-                        @click="selected = mediaItem" clickable v-ripple>
-                    <q-item-label class="row items-center">
-                        <span>{{trimTitle(mediaItem?.media?.title)}}</span>
-                    </q-item-label>
+            <q-list style="height: 500px" separator>
+                <q-item>
+                    <q-item-section>
+                        <q-input type="search" v-model="search" label="Search by Title" dense outlined>
+                            <template #append>
+                                <q-btn v-show="Boolean(search)" icon="close" size="md" @click="search = ''" flat round />
+                                <q-btn icon="search" flat round />
+                            </template>
+                        </q-input>
+                    </q-item-section>
                 </q-item>
+                <transition-group name="list" mode="out-in" appear>
+                    <q-item v-for="mediaItem in computedList"
+                            active-class="bg-primary text-white"
+                            :active="mediaItem === selected"
+                            @click="selected = mediaItem" clickable v-ripple>
+                        <q-item-label class="row items-center">
+                            <span>{{trimTitle(mediaItem?.media?.title)}}</span>
+                        </q-item-label>
+                    </q-item>
+                </transition-group>
             </q-list>
         </div>
         <div v-if="selected" class="col-8 col-md-9 col-lg-10 row justify-center items-center">
@@ -58,6 +75,7 @@ function trimTitle(title) {
 }
 .left {
     border-right: 1px solid rgba(0, 0, 0, 0.28);
+    overflow-x: hidden;
     overflow-y: auto;
 }
 </style>
