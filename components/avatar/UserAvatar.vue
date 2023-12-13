e63e375835dcf499bc9d3d79a1aa77838383c7b2
zlamb1 <zlamb@my.wctc.edu>
2023-12-12 20:07:56 -0600

    fixed user avatar

--- components/avatar/UserAvatar.vue
+++ components/avatar/UserAvatar.vue
@@ -1,6 +1,4 @@
 <script setup>
-const hovering = ref(false);
-
 const props = defineProps({
     displayName: {
         type: String,
@@ -8,22 +6,22 @@ const props = defineProps({
     photoUrl: {
         type: String,
     },
-    hoverColor: {
-        type: String,
-    },
 });
 
-const computedStyle = computed(() => {
-    return hovering.value ? `background: ${props.hoverColor} !important` : null;
+const error = ref(false);
+
+watch(() => props.photoUrl, () => {
+   error.value = false;
 });
 </script>
 
 <template>
-    <q-avatar v-bind="$attrs" size="md" color="primary" text-color="white" @mouseover="hovering = true" @mouseleave="hovering = false" rounded>
-        <div class="fit absolute rounded-borders" :style="computedStyle" />
-        <q-img v-if="photoUrl"
+    <q-avatar v-bind="$attrs" size="md" :color="photoUrl && !error ? 'transparent' : 'primary'" text-color="white" rounded>
+        <q-img v-if="photoUrl && !error"
+               referrerpolicy="no-referrer"
                :src="photoUrl"
-               referrerpolicy="no-referrer">
+               @load="error = false"
+               @error="error = true">
         </q-img>
         <span v-else class="non-selectable">{{displayName?.substring(0,1)}}</span>
         <slot></slot>
